29880d87a052678b59503b386513546357e10d9a
Lavenda Watila <lavenda@Lavendas-MacBook-Air.local>
2022-01-27 17:56:12 +0300

    first commit

---
+++ Variables.php
@@ -0,0 +1,48 @@
+<?php
+$majina="Lavenda Watila";
+$MAJINA="Eric Were";
+
+echo" My Name is $majina";
+
+echo"<br>";
+
+$nambari1=987;
+$nambari2=563;
+
+echo "The Sum is :.....";
+echo$nambari1+$nambari2;
+
+
+
+//put a break
+echo"<br>";
+
+//the difference
+$nambari1=987;
+$nambari2=563;
+
+echo "The difference is :......";
+echo$nambari1-$nambari2;
+
+
+
+//multiplication
+$nambari1=987;
+$nambari2=563;
+
+echo"<br>";
+echo "The Multiplication is:.....";
+
+echo$nambari1*$nambari2;
+
+
+
+//Division
+$nambari1=987;
+$nambari2=563;
+
+echo"<br>";
+echo "The Division is:.....";
+echo$nambari1/$nambari2;
+
+echo"<br>";
---
+++ intro.php
@@ -0,0 +1,29 @@
+<?php
+echo "Welcome to php explorer Web Development Class";
+echo"<br>";
+echo "This is My First Class in Php";
+echo"<br>";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php</title>
+</head>
+<body>
+<?php
+
+echo"How is php taking you so far?";
+echo"<br>";
+echo 56 + 39;
+echo"<br>";
+
+
+?>
+</body>
+</html>
+
